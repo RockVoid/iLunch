dee9748afc36b2cc8a5ea1c672550f7e28224274
JonaSPyt <jonasemanuel117@gmail.com>
2022-05-03 18:49:38 -0300

    Correção de bug no profile

--- lib/pages/Profile.dart
+++ lib/pages/Profile.dart
@@ -37,184 +37,185 @@ class Profile extends StatelessWidget {
             )
           ],
         ),
-        body: Center(
-            child: Padding(
-          padding: EdgeInsets.all(15.0),
-          child: Column(children: [
-            Container(
-              width: 200.0,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: CircleAvatar(
-                  backgroundImage: NetworkImage(
-                      "https://imagens3.ne10.uol.com.br/blogsne10/social1/uploads/2019/11/Faust%C3%A3o-se-irrita-com-funcion%C3%A1rio-ao-vivo.jpg"),
-                  radius: 60,
+        body: ListView(children: [
+          Padding(
+            padding: EdgeInsets.all(15.0),
+            child: Column(children: [
+              Container(
+                width: 200.0,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: CircleAvatar(
+                    backgroundImage: NetworkImage(
+                        "https://imagens3.ne10.uol.com.br/blogsne10/social1/uploads/2019/11/Faust%C3%A3o-se-irrita-com-funcion%C3%A1rio-ao-vivo.jpg"),
+                    radius: 60,
+                  ),
                 ),
+                decoration: new BoxDecoration(
+                    shape: BoxShape.circle,
+                    border: Border.all(color: Colors.red, width: 2.0)),
               ),
-              decoration: new BoxDecoration(
-                  shape: BoxShape.circle,
-                  border: Border.all(color: Colors.red, width: 2.0)),
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
-            Text(
-              "Guilherme Alencar",
-              style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black,
-                  fontSize: 24),
-            ),
-            Text(
-              "guilhermealencar@gmail.com",
-              style: TextStyle(color: Colors.grey.shade800, fontSize: 12),
-            ),
-            Text(
-              "+91 xxxxxxxxxx",
-              style: TextStyle(color: Colors.grey.shade800, fontSize: 12),
-            ),
-            Text(
-              "Rua do Benfica, 777",
-              style: TextStyle(color: Colors.grey.shade800, fontSize: 12),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                TextButton.icon(
-                  icon: Icon(Icons.tab),
-                  onPressed: () {
-                    Navigator.of(context)
-                        .push(MaterialPageRoute(builder: (context) => Saves()));
-                  },
-                  label: Text("Salvos"),
-                  style: TextButton.styleFrom(elevation: 0.8),
-                ),
-                TextButton.icon(
-                  icon: Icon(Icons.notification_important_outlined),
-                  onPressed: () {
-                    Navigator.of(context).push(MaterialPageRoute(
-                        builder: (context) => Notifications()));
-                  },
-                  label: Text("Notificações"),
-                  style: TextButton.styleFrom(elevation: 0.8),
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 80,
-            ),
-            SizedBox(
-                height: 60,
-                width: 300,
-                child: OutlinedButton.icon(
-                  onPressed: () {
-                    Navigator.of(context).push(
-                        MaterialPageRoute(builder: (context) => Historic()));
-                  },
-                  icon: Text(
-                    "Historico de Pedidos",
-                    style: TextStyle(
-                      fontSize: 20,
+              SizedBox(
+                height: 20.0,
+              ),
+              Text(
+                "Guilherme Alencar",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                    fontSize: 24),
+              ),
+              Text(
+                "guilhermealencar@gmail.com",
+                style: TextStyle(color: Colors.grey.shade800, fontSize: 12),
+              ),
+              Text(
+                "+91 xxxxxxxxxx",
+                style: TextStyle(color: Colors.grey.shade800, fontSize: 12),
+              ),
+              Text(
+                "Rua do Benfica, 777",
+                style: TextStyle(color: Colors.grey.shade800, fontSize: 12),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  TextButton.icon(
+                    icon: Icon(Icons.tab),
+                    onPressed: () {
+                      Navigator.of(context).push(
+                          MaterialPageRoute(builder: (context) => Saves()));
+                    },
+                    label: Text("Salvos"),
+                    style: TextButton.styleFrom(elevation: 0.8),
+                  ),
+                  TextButton.icon(
+                    icon: Icon(Icons.notification_important_outlined),
+                    onPressed: () {
+                      Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) => Notifications()));
+                    },
+                    label: Text("Notificações"),
+                    style: TextButton.styleFrom(elevation: 0.8),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 80,
+              ),
+              SizedBox(
+                  height: 60,
+                  width: 300,
+                  child: OutlinedButton.icon(
+                    onPressed: () {
+                      Navigator.of(context).push(
+                          MaterialPageRoute(builder: (context) => Historic()));
+                    },
+                    icon: Text(
+                      "Historico de Pedidos",
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
                     ),
-                  ),
-                  label: Icon(
-                    Icons.arrow_forward_ios_outlined,
-                    size: 18,
-                  ),
-                  style: OutlinedButton.styleFrom(
-                      primary: Colors.black, elevation: 1),
-                )),
-            SizedBox(
-              height: 30,
-            ),
-            SizedBox(
-                height: 60,
-                width: 300,
-                child: OutlinedButton.icon(
-                  onPressed: () {
-                    Navigator.of(context)
-                        .push(MaterialPageRoute(builder: (context) => Help()));
-                  },
-                  label: Icon(Icons.arrow_forward_ios_outlined, size: 18),
-                  icon: Text(
-                    "Ajuda",
-                    textAlign: TextAlign.left,
-                    style: TextStyle(fontSize: 20),
-                  ),
-                  style: OutlinedButton.styleFrom(
-                      primary: Colors.black, elevation: 1),
-                )),
-            SizedBox(
-              height: 40,
-            ),
-            RichText(
-              text: TextSpan(
-                children: <TextSpan>[
-                  TextSpan(
-                      text: ' Enviar um Feedback',
-                      recognizer: TapGestureRecognizer()
-                        ..onTap = () => {
-                              Navigator.of(context).push(MaterialPageRoute(
-                                  builder: (context) => Feedbacks()))
-                            },
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black,
-                          fontSize: 16))
-                ],
+                    label: Icon(
+                      Icons.arrow_forward_ios_outlined,
+                      size: 18,
+                    ),
+                    style: OutlinedButton.styleFrom(
+                        primary: Colors.black, elevation: 1),
+                  )),
+              SizedBox(
+                height: 30,
               ),
-            ),
-            SizedBox(
-              height: 17,
-            ),
-            RichText(
-              text: TextSpan(
-                children: <TextSpan>[
-                  TextSpan(
-                      text: ' Reportar Algo',
-                      recognizer: TapGestureRecognizer()
-                        ..onTap = () => {
-                              Navigator.of(context).push(MaterialPageRoute(
-                                  builder: (context) => Report()))
-                            },
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black,
-                          fontSize: 16))
-                ],
+              SizedBox(
+                  height: 60,
+                  width: 300,
+                  child: OutlinedButton.icon(
+                    onPressed: () {
+                      Navigator.of(context).push(
+                          MaterialPageRoute(builder: (context) => Help()));
+                    },
+                    label: Icon(Icons.arrow_forward_ios_outlined, size: 18),
+                    icon: Text(
+                      "Ajuda",
+                      textAlign: TextAlign.left,
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    style: OutlinedButton.styleFrom(
+                        primary: Colors.black, elevation: 1),
+                  )),
+              SizedBox(
+                height: 40,
               ),
-            ),
-            SizedBox(
-              height: 17,
-            ),
-            RichText(
-              text: TextSpan(
-                children: <TextSpan>[
-                  TextSpan(
-                      text: ' Avalie-nos',
-                      recognizer: TapGestureRecognizer()
-                        ..onTap = () => {
-                              Navigator.of(context).push(MaterialPageRoute(
-                                  builder: (context) => Evaluate()))
-                            },
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black,
-                          fontSize: 16))
-                ],
+              RichText(
+                text: TextSpan(
+                  children: <TextSpan>[
+                    TextSpan(
+                        text: ' Enviar um Feedback',
+                        recognizer: TapGestureRecognizer()
+                          ..onTap = () => {
+                                Navigator.of(context).push(MaterialPageRoute(
+                                    builder: (context) => Feedbacks()))
+                              },
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                            fontSize: 16))
+                  ],
+                ),
               ),
-            ),
-            SizedBox(
-              height: 17,
-            ),
-            SizedBox(
-              height: 6,
-            ),
-          ]),
-        )),
+              SizedBox(
+                height: 17,
+              ),
+              RichText(
+                text: TextSpan(
+                  children: <TextSpan>[
+                    TextSpan(
+                        text: ' Reportar Algo',
+                        recognizer: TapGestureRecognizer()
+                          ..onTap = () => {
+                                Navigator.of(context).push(MaterialPageRoute(
+                                    builder: (context) => Report()))
+                              },
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                            fontSize: 16))
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 17,
+              ),
+              RichText(
+                text: TextSpan(
+                  children: <TextSpan>[
+                    TextSpan(
+                        text: ' Avalie-nos',
+                        recognizer: TapGestureRecognizer()
+                          ..onTap = () => {
+                                Navigator.of(context).push(MaterialPageRoute(
+                                    builder: (context) => Evaluate()))
+                              },
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                            fontSize: 16))
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 17,
+              ),
+              SizedBox(
+                height: 6,
+              ),
+            ]),
+          ),
+        ]),
       );
 }
