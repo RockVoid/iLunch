98ae03026fb06ce02fc23d3e7f57db9f7f40eb60
geovanereboucas <geovane.reboucas2002@gmail.com>
2022-05-11 00:19:24 -0300

    #fix Modificando o estilo da tela de perfil do cliente

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:ilunch/pages/auth_page.dart';
 import 'package:ilunch/pages/edit_page.dart';
 import 'package:ilunch/pages/feedback_page.dart';
 import 'package:ilunch/pages/help.dart';
@@ -8,25 +9,39 @@ import 'package:ilunch/pages/historic_page.dart';
 import 'package:ilunch/pages/notifications_page.dart';
 import 'package:ilunch/pages/report_page.dart';
 import 'package:ilunch/pages/saves.dart';
+import 'package:ilunch/services/auth_methods.dart';
 import 'package:ilunch/themes/app_themes.dart';
+import 'package:ilunch/widgets/show_snack_bar.dart';
 
-class ProfilePage extends StatelessWidget {
+class ProfilePage extends StatefulWidget {
+  @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  void signOut() async {
+    await AuthMethods().signOut();
+    Navigator.of(context).pushReplacement(
+      MaterialPageRoute(
+        builder: (context) => AuthPage(),
+      ),
+    );
+    ShowSnackBar('Usuário desconectado', context);
+  }
+
   @override
   Widget build(BuildContext context) => Scaffold(
         appBar: AppBar(
           elevation: 0,
           centerTitle: true,
           automaticallyImplyLeading: false,
-          backgroundColor: Colors.white,
-          titleSpacing: 0.0,
-          title: Transform(
-            transform: Matrix4.translationValues(0.0, 0.0, 0.0),
-            child: Text(
-              "Detalhes Pessoais",
-              style: GoogleFonts.roboto(
-                  textStyle: TextStyle(
-                      color: Colors.black, fontWeight: FontWeight.bold)),
-            ),
+          backgroundColor: Color.fromARGB(245, 250, 250, 250),
+          titleSpacing: 14,
+          title: Text(
+            "Detalhes Pessoais",
+            style: GoogleFonts.roboto(
+                textStyle: TextStyle(
+                    color: Colors.black, fontWeight: FontWeight.bold)),
           ),
           actions: [
             IconButton(
@@ -41,48 +56,54 @@ class ProfilePage extends StatelessWidget {
         ),
         body: ListView(children: [
           Padding(
-            padding: EdgeInsets.all(15.0),
-            child: Column(children: [
-              Container(
-                width: 200.0,
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: CircleAvatar(
-                    backgroundImage: NetworkImage(
-                        "https://imagens3.ne10.uol.com.br/blogsne10/social1/uploads/2019/11/Faust%C3%A3o-se-irrita-com-funcion%C3%A1rio-ao-vivo.jpg"),
-                    radius: 60,
-                  ),
+            padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              Center(
+                child: Column(
+                  children: [
+                    Container(
+                      alignment: AlignmentDirectional.center,
+                      width: 200.0,
+                      child: Padding(
+                        padding: const EdgeInsets.all(5),
+                        child: ClipRRect(
+                            borderRadius: BorderRadius.circular(30),
+                            child: Image.network(
+                              "https://imagens3.ne10.uol.com.br/blogsne10/social1/uploads/2019/11/Faust%C3%A3o-se-irrita-com-funcion%C3%A1rio-ao-vivo.jpg",
+                                height: 150,
+                            )),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 16,
+                    ),
+                    Text(
+                      "Guilherme Alencar",
+                      style: GoogleFonts.poppins(
+                          textStyle: TextStyle(
+                              fontWeight: FontWeight.w600,
+                              color: Colors.black,
+                              fontSize: 21)),
+                    ),
+                    Text(
+                      "guilhermealencar@gmail.com",
+                      style: GoogleFonts.poppins(
+                          textStyle: TextStyle(
+                              color: Colors.grey.shade800, fontSize: 11)),
+                    ),
+                    Text(
+                      "Rua do Benfica, 777",
+                      style: GoogleFonts.poppins(
+                          textStyle: TextStyle(
+                              color: Colors.grey.shade800, fontSize: 12)),
+                    ),
+                    Text("+91 xxxxxxxxxx",
+                        style: GoogleFonts.poppins(
+                            textStyle: TextStyle(
+                                color: Colors.grey.shade800, fontSize: 12))),
+                  ],
                 ),
-                decoration: new BoxDecoration(
-                    shape: BoxShape.circle,
-                    border: Border.all(color: Appthemes.primary, width: 2.0)),
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              Text(
-                "Guilherme Alencar",
-                style: GoogleFonts.roboto(
-                    textStyle: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black,
-                        fontSize: 24)),
-              ),
-              Text(
-                "guilhermealencar@gmail.com",
-                style: GoogleFonts.roboto(
-                    textStyle:
-                        TextStyle(color: Colors.grey.shade800, fontSize: 12)),
-              ),
-              Text("+91 xxxxxxxxxx",
-                  style: GoogleFonts.roboto(
-                      textStyle: TextStyle(
-                          color: Colors.grey.shade800, fontSize: 12))),
-              Text(
-                "Rua do Benfica, 777",
-                style: GoogleFonts.roboto(
-                    textStyle:
-                        TextStyle(color: Colors.grey.shade800, fontSize: 12)),
               ),
               SizedBox(
                 height: 20,
@@ -128,63 +149,97 @@ class ProfilePage extends StatelessWidget {
                 ],
               ),
               SizedBox(
-                height: 80,
+                height: 20,
               ),
-              SizedBox(
-                  height: 60,
-                  width: 300,
-                  child: OutlinedButton.icon(
-                    onPressed: () {
-                      Navigator.of(context).push(MaterialPageRoute(
-                          builder: (context) => HistoricPage()));
-                    },
-                    icon: Text(
-                      "Histórico de Pedidos",
-                      style: GoogleFonts.poppins(
-                          textStyle: TextStyle(
-                        fontSize: 20,
-                      )),
-                    ),
-                    label: Icon(
-                      Icons.arrow_forward_ios_outlined,
-                      size: 18,
-                    ),
-                    style: OutlinedButton.styleFrom(
+              Row(
+                children: [
+                  Expanded(
+                    child: OutlinedButton(
+                      onPressed: () {
+                        Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) => HistoricPage()));
+                      },
+                      style: OutlinedButton.styleFrom(
+                        shape: StadiumBorder(),
                         primary: Colors.black,
                         elevation: 1,
-                        backgroundColor: Colors.white),
-                  )),
+                        backgroundColor: Colors.white,
+                        padding: EdgeInsets.symmetric(
+                          vertical: 13,
+                          horizontal: 22,
+                        ),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "Histórico de Pedidos",
+                            style: GoogleFonts.poppins(
+                              textStyle: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ),
+                          Icon(
+                            Icons.arrow_forward_ios_outlined,
+                            size: 20,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
               SizedBox(
                 height: 30,
               ),
-              SizedBox(
-                  height: 60,
-                  width: 300,
-                  child: OutlinedButton.icon(
-                    onPressed: () {
-                      Navigator.of(context).push(
-                          MaterialPageRoute(builder: (context) => Help()));
-                    },
-                    label: Icon(Icons.arrow_forward_ios_outlined, size: 18),
-                    icon: Text(
-                      "Ajuda",
-                      textAlign: TextAlign.left,
-                      style: GoogleFonts.poppins(
-                          textStyle: TextStyle(fontSize: 20)),
-                    ),
-                    style: OutlinedButton.styleFrom(
+              Row(
+                children: [
+                  Expanded(
+                    child: OutlinedButton(
+                      onPressed: () {
+                        Navigator.of(context).push(
+                            MaterialPageRoute(builder: (context) => Help()));
+                      },
+                      style: OutlinedButton.styleFrom(
+                        shape: StadiumBorder(),
                         primary: Colors.black,
                         elevation: 1,
-                        backgroundColor: Colors.white),
-                  )),
+                        backgroundColor: Colors.white,
+                        padding:
+                            EdgeInsets.symmetric(vertical: 13, horizontal: 22),
+                      ),
+                      child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              "Ajuda",
+                              style: GoogleFonts.poppins(
+                                textStyle: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                            ),
+                            Icon(
+                              Icons.arrow_forward_ios_outlined,
+                              size: 20,
+                            ),
+                          ]),
+                    ),
+                  ),
+                ],
+              ),
               SizedBox(
                 height: 40,
               ),
               RichText(
+                textAlign: TextAlign.start,
                 text: TextSpan(
                   children: <TextSpan>[
                     TextSpan(
-                        text: ' Enviar um Feedback',
+                        text: 'Enviar um Feedback',
                         recognizer: TapGestureRecognizer()
                           ..onTap = () => {
                                 Navigator.of(context).push(MaterialPageRoute(
@@ -192,7 +247,7 @@ class ProfilePage extends StatelessWidget {
                               },
                         style: GoogleFonts.poppins(
                             textStyle: TextStyle(
-                                fontWeight: FontWeight.bold,
+                                fontWeight: FontWeight.w400,
                                 color: Colors.black,
                                 fontSize: 16)))
                   ],
@@ -205,7 +260,7 @@ class ProfilePage extends StatelessWidget {
                 text: TextSpan(
                   children: <TextSpan>[
                     TextSpan(
-                        text: ' Reportar Algo',
+                        text: 'Reportar Algo',
                         recognizer: TapGestureRecognizer()
                           ..onTap = () => {
                                 Navigator.of(context).push(MaterialPageRoute(
@@ -213,7 +268,7 @@ class ProfilePage extends StatelessWidget {
                               },
                         style: GoogleFonts.poppins(
                             textStyle: TextStyle(
-                                fontWeight: FontWeight.bold,
+                                fontWeight: FontWeight.w400,
                                 color: Colors.black,
                                 fontSize: 16)))
                   ],
@@ -226,21 +281,32 @@ class ProfilePage extends StatelessWidget {
                 text: TextSpan(
                   children: <TextSpan>[
                     TextSpan(
-                        text: ' Avalie-nos',
+                        text: 'Avalie-nos',
                         recognizer: TapGestureRecognizer()..onTap = () => {},
                         style: GoogleFonts.poppins(
                             textStyle: TextStyle(
-                                fontWeight: FontWeight.bold,
+                                fontWeight: FontWeight.w400,
                                 color: Colors.black,
                                 fontSize: 16)))
                   ],
                 ),
               ),
               SizedBox(
-                height: 17,
+                height: 16,
               ),
-              SizedBox(
-                height: 6,
+              RichText(
+                text: TextSpan(
+                  children: <TextSpan>[
+                    TextSpan(
+                        text: 'Sair',
+                        recognizer: TapGestureRecognizer()..onTap = signOut,
+                        style: GoogleFonts.poppins(
+                            textStyle: TextStyle(
+                                fontWeight: FontWeight.w500,
+                                color: Appthemes.primary,
+                                fontSize: 16)))
+                  ],
+                ),
               ),
             ]),
           ),
