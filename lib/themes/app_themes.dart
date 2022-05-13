21d22f9da731422bbf832c34996016fd5cf99254
geovanereboucas <geovane.reboucas2002@gmail.com>
2022-05-12 22:20:47 -0300

    #feat Adicionando tela de histórico de pedidos do usuário.

--- lib/themes/app_themes.dart
+++ lib/themes/app_themes.dart
@@ -5,6 +5,7 @@ class Appthemes {
   static final primary = Color(0xFFFFEA1D2C);
   static final background = Color(0xFFFFFFFF);
   static final stroke = Color(0xFFE3E3E6);
+  static final greySubtitle = Color(0xFF706E7A);
 
   //images
 
