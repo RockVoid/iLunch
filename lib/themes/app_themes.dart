4f903ce790e5e2d359209cf61a11dde9e329a179
geovanereboucas <geovane.reboucas2002@gmail.com>
2022-05-19 11:18:17 -0300

    #fix Configurando widgets e dependencias

--- lib/themes/app_themes.dart
+++ lib/themes/app_themes.dart
@@ -6,6 +6,7 @@ class Appthemes {
   static final background = Color(0xFFFFFFFF);
   static final stroke = Color(0xFFE3E3E6);
   static final greySubtitle = Color(0xFF706E7A);
+  static final avaliationColor = Color(0xFFF9B405);
 
   //images
 
--- lib/widgets/home_item_vendedor.dart
+++ lib/widgets/home_item_vendedor.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:ilunch/model/vendedor_model.dart';
 import 'package:ilunch/pages/home_page.dart';
+import 'package:ilunch/pages/loja_page.dart';
 import 'package:ilunch/themes/app_themes.dart';
 
 class HomeItemVendedor extends StatelessWidget {
@@ -16,59 +17,68 @@ class HomeItemVendedor extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              ClipRRect(
-                borderRadius: BorderRadius.circular(100),
-                child: Image.network(
-                  vendedor.image,
-                  height: 60,
-                  width: 60,
-                  fit: BoxFit.cover,
+          InkWell(
+            onTap: () {
+              if (vendedor.name == 'Divina Picanha') {
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => Loja()));
+                print('foi');
+              }
+            },
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(100),
+                  child: Image.network(
+                    vendedor.image,
+                    height: 60,
+                    width: 60,
+                    fit: BoxFit.cover,
+                  ),
                 ),
-              ),
-              SizedBox(
-                width: 10,
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  SizedBox(
-                    height: 3,
-                  ),
-                  Row(
-                    children: [
-                      Text(
-                        vendedor.name,
-                        style: GoogleFonts.poppins(
-                          fontWeight: FontWeight.w500,
+                SizedBox(
+                  width: 10,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      height: 3,
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          vendedor.name,
+                          style: GoogleFonts.poppins(
+                            fontWeight: FontWeight.w500,
+                          ),
                         ),
-                      ),
-                      SizedBox(width: 5),
-                      Icon(
-                        Icons.verified,
-                        color: Appthemes.primary,
-                        size: 18,
-                      ),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Text(
-                        vendedor.stars,
-                        style: GoogleFonts.poppins(),
-                      ),
-                      Icon(
-                        Icons.star_rate_rounded,
-                        color: Color(0xFFFFF9B405),
-                      ),
-                      Text('• ${vendedor.category}')
-                    ],
-                  )
-                ],
-              ),
-            ],
+                        SizedBox(width: 5),
+                        Icon(
+                          Icons.verified,
+                          color: Appthemes.primary,
+                          size: 18,
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          vendedor.stars,
+                          style: GoogleFonts.poppins(),
+                        ),
+                        Icon(
+                          Icons.star_rate_rounded,
+                          color: Color(0xFFFFF9B405),
+                        ),
+                        Text('• ${vendedor.category}')
+                      ],
+                    )
+                  ],
+                ),
+              ],
+            ),
           ),
           IconButton(
             onPressed: () {},
